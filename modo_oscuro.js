908f4c2eec0610a0181a8992e81d57b92c86ef6f
Veneiro <50624734+Veneiro@users.noreply.github.com>
2024-06-02 19:41:12 +0200

    dispatch

--- modo_oscuro.js
+++ modo_oscuro.js
@@ -1,20 +1,22 @@
-const toggleThemeButton = document.getElementById('toggle-theme');
-const body = document.body;
+window.addEventListener('headerLoaded', () => {
+  const toggleThemeButton = document.getElementById('toggle-theme');
+  const body = document.body;
 
-// Comprueba si el modo oscuro está guardado en localStorage y actualiza la interfaz de usuario en consecuencia
-if (localStorage.getItem('dark-mode') === 'true') {
-  body.classList.add('dark-mode');
-  toggleThemeButton.textContent = '🌙';
-}
-
-toggleThemeButton.addEventListener('click', () => {
-  if (body.classList.contains('dark-mode')) {
-    body.classList.remove('dark-mode');
-    toggleThemeButton.textContent = '☀️';
-    localStorage.setItem('dark-mode', 'false');
-  } else {
+  // Comprueba si el modo oscuro está guardado en localStorage y actualiza la interfaz de usuario en consecuencia
+  if (localStorage.getItem('dark-mode') === 'true') {
     body.classList.add('dark-mode');
     toggleThemeButton.textContent = '🌙';
-    localStorage.setItem('dark-mode', 'true');
   }
+
+  toggleThemeButton.addEventListener('click', () => {
+    if (body.classList.contains('dark-mode')) {
+      body.classList.remove('dark-mode');
+      toggleThemeButton.textContent = '☀️';
+      localStorage.setItem('dark-mode', 'false');
+    } else {
+      body.classList.add('dark-mode');
+      toggleThemeButton.textContent = '🌙';
+      localStorage.setItem('dark-mode', 'true');
+    }
+  });
 });
